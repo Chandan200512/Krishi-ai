812e472d213c26b16221d220403247f2c808410d
Chandan NG <prince.chandugowda07@gmail.com>
2025-10-27 12:44:00 +0530

    Use environment variable for server port

--- simple-server.js
+++ simple-server.js
@@ -15,7 +15,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'public', 'index.html'));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, '0.0.0.0', () => {
   console.log('🚀 Krishi AI server running on http://localhost:5000');
   console.log('Your agricultural platform is ready!');
